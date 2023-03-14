c432899a1a29a26ebc102973406d2216ad6b534d
sollendy <edlannino@tutanota.com>
2023-03-14 15:50:05 +0100

    fix: problema con rendere elementi del array

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const createApp = Vue;
+const { createApp } = Vue;
 
 createApp({
     data() {
@@ -8,22 +8,26 @@ createApp({
                 image: 'img/01.webp',
                 title: 'Marvel\'s Spiderman Miles Morale',
                 text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-                }, 
+                },
+
                 {
                 image: 'img/02.webp',
                 title: 'Ratchet & Clank: Rift Apart',
                 text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-                }, 
+                },
+
                 {
                 image: 'img/03.webp',
                 title: 'Fortnite',
                 text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-                }, 
+                },
+
                 {
                 image: 'img/04.webp',
                 title: 'Stray',
                 text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-                }, 
+                },
+
                 {
                 image: 'img/05.webp',
                 title: "Marvel's Avengers",
@@ -31,9 +35,9 @@ createApp({
                 }
             ],
 
+            slideAttiva: 0,
         }
     },
-    slideAttiva: 0,
 
 
     methods: {
